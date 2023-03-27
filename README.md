278fd9183fdd4a7c75e358cfe803b69ddfafbb10
Mertna <darck.patatoes@gmail.com>
2023-03-27 15:34:16 +0200

    uploaded project
    uploaded project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Dark & Light Mode Switch
+### mmhhmmmm dark an light mod
+Stores user's choice in localstorage and dynamically adapt iframe's content
---
+++ index.js
@@ -0,0 +1,15 @@
+
+	const currentTheme = localStorage.getItem("theme");
+	const stylesheet = document.querySelector("#stylesheet");
+	var btn = document.querySelector(".but");
+	var theme = "light";
+	
+	function onInit(){ if (currentTheme == "dark") { toggleTheme(); } };
+	
+	function toggleTheme() {
+		if (theme == "dark" || currentTheme == null) {	stylesheet.href = "index-light.css"; theme = "light"; }
+												else {	stylesheet.href = "index-dark.css";	 theme = "dark";	}
+		localStorage.setItem("theme", theme);
+		document.querySelector(".but").textContent = "mhhmmm " + theme + " mode";
+		document.querySelector("#framer").contentWindow.location.reload();
+	};
