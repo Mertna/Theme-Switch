8f5fe62215eb6f952ca57e3b0dd0ddc403e208cc
Mertna <darck.patatoes@gmail.com>
2023-03-28 12:45:37 +0200

    localstorage init on first visit

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
 	var btn = document.querySelector(".but");
 	var theme = "light";
 	
-	function onInit(){ if (currentTheme == "dark") { toggleTheme(); } };
+	function onInit(){
+		if (currentTheme != "dark" && currentTheme != "light") { localStorage.setItem("theme", "light"); }
+		if (currentTheme == "dark") { toggleTheme(); }
+	};
 	
 	function toggleTheme() {
 		if (theme == "dark" || currentTheme == null) {	stylesheet.href = "index-light.css"; theme = "light"; }
-												else {	stylesheet.href = "index-dark.css";	 theme = "dark";	}
+							else {	stylesheet.href = "index-dark.css";	 theme = "dark"; }
 		localStorage.setItem("theme", theme);
 		document.querySelector(".but").textContent = "mhhmmm " + theme + " mode";
 		document.querySelector("#framer").contentWindow.location.reload();
