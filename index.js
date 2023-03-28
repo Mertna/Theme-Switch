417142d39732ced681c2f9295e880704b602bae2
Mertna <darck.patatoes@gmail.com>
2023-03-28 13:00:05 +0200

    fixed localstorage conflict on init

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 	var btn = document.querySelector(".but");
 	var theme = "light";
 	
-	function onInit(){  if (localStorage.getItem("theme") == "dark") { toggleTheme(); }  };
+	function onInit(){
+		if (localStorage.getItem("theme") == null) { localStorage.setItem("theme", theme); }
+		if (localStorage.getItem("theme") == "dark") { toggleTheme(); }
+	};
 	
 	function toggleTheme() {
-		if (theme == "dark" || localStorage.getItem("theme") == null) {	stylesheet.href = "index-light.css"; theme = "light"; }
-									 else {	stylesheet.href = "index-dark.css"; theme = "dark"; }
+		if (theme == "dark") {  stylesheet.href = "index-light.css"; theme = "light"; }
+				else {	stylesheet.href = "index-dark.css"; theme = "dark"; }
 		localStorage.setItem("theme", theme);
 		document.querySelector(".but").textContent = "mhhmmm " + theme + " mode";
 		document.querySelector("#framer").contentWindow.location.reload();
