403b41ca894ee59ee9f017f71cd61f4ec8056698
Mertna <darck.patatoes@gmail.com>
2023-03-28 12:53:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-
-	const currentTheme = localStorage.getItem("theme");
 	const stylesheet = document.querySelector("#stylesheet");
 	var btn = document.querySelector(".but");
 	var theme = "light";
 	
-	function onInit(){
-		if (currentTheme != "dark" && currentTheme != "light") { localStorage.setItem("theme", "light"); }
-		if (currentTheme == "dark") { toggleTheme(); }
-	};
+	function onInit(){  if (localStorage.getItem("theme") == "dark") { toggleTheme(); }  };
 	
 	function toggleTheme() {
-		if (theme == "dark" || currentTheme == null) {	stylesheet.href = "index-light.css"; theme = "light"; }
-							else {	stylesheet.href = "index-dark.css";	 theme = "dark"; }
+		if (theme == "dark" || localStorage.getItem("theme") == null) {	stylesheet.href = "index-light.css"; theme = "light"; }
+									 else {	stylesheet.href = "index-dark.css"; theme = "dark"; }
 		localStorage.setItem("theme", theme);
 		document.querySelector(".but").textContent = "mhhmmm " + theme + " mode";
 		document.querySelector("#framer").contentWindow.location.reload();
